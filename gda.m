beee18e04635604a5cd6151d1b164418a36bff21
yannCPM <yann.le-poul@mnhn.fr>
2016-05-25 17:33:07 +0200

    add vectorization to some of the kernel calculation , which makes computation much faster

--- gda.m
+++ gda.m
@@ -82,14 +82,7 @@ clear trainLabel
 kTrainCell = cell(c,c);
 for p = 1:c
     for q = 1:c
-        Kpq = zeros(nSample(p),nSample(q));
-        classP = dataCell{1,p};
-        classQ = dataCell{1,q};
-        for i = 1:nSample(p)
-            for j = 1:nSample(q)
-                Kpq(i,j) = kernel(classP(:,i),classQ(:,j),options);
-            end
-        end
+        Kpq = f_kernel(dataCell{1,p},dataCell{1,q},options);
         kTrainCell{p,q} = Kpq;
     end
 end
@@ -193,14 +186,7 @@ w = alpha(:,1:nDim);    % Projection matrix
 [~,nPrime] = size(data);
 kDataCell = cell(c,1);
 for p = 1:c
-    Kp = zeros(nSample(p),nPrime);
-    classP = dataCell{1,p};
-    for i = 1:nSample(p)
-        for j = 1:nPrime
-            Kp(i,j) = kernel(classP(:,i),data(:,j),options);
-        end
-    end
-    kDataCell{p,1} = Kp;
+    kDataCell{p,1} = f_kernel(dataCell{1,p},data,options);
 end
 kData = cell2mat(kDataCell);
 clear data dataCell kDataCell
@@ -216,3 +202,170 @@ clear kTrain kData
 % Project all data points non-linearly onto a new lower-dimensional subspace (w):
 
 mappedData = (w.') * (zeroMeanKdata);
+
+end
+
+
+
+function k = f_kernel(u,v,options)
+
+% KERNEL determines kernel function for kernel-based machine learning 
+% methods including Support Vector Machines.
+% 
+% 
+% 
+% Inputs:
+%           u:          First input vector (p dimensional column vector)
+%           v:          Second input vector (p dimensional column vector)
+%           options:    Struct value in Matlab
+%                       The fields in options that can be set:
+% 
+%                       options.KernelType:	choices are:
+% 
+%                           'linear'        without kernel function (no parameter)(default value)
+%                           'poly'          simple polynomial kernel function (with 1 parameter which is degree of polynomial)
+%                           'polyplus'      polynomial kernel function (with 1 parameter which is degree of polynomial)
+%                           'sphnorm_poly'  spherically normalized polynomial kernel function (with 2 parameters: 
+%                                           the first one is degree of polynomial & the second one is spherical normalization parameter)
+%                           'rbf'           Gaussian (RBF) kernel function (with 1 parameyer which is width of RBF (sigma)
+%                           'wave'          wavelet kernel function (with 1 parameter which is dilation factor. 
+%                       
+%                       options.KernelPars: a row vector. Minimum size of it is 0 (when no parameters is needed for kernel function)
+%                                           and maximum size of it is 2 (when kernel function has 2 parameters)
+%
+%
+% Output:
+%           k:      kernel function value (a real scalar)
+% 
+% 
+% Sample use:    
+%       k = kernel(u,v,options);     % computes dot product in feature space
+% 
+% 
+% 
+% Bugs: (1) Before using this function, you should determine suitable values for options.KernelType and options.KernelPars 
+%           variables otherwise the default values will be used by function.
+%       (2) If you use spherically normalized kernels, you'd better already normalize the training data to zero mean and unit variance then set the spherical
+%           normalization parameter to 1.
+%
+% 
+% 
+%   More details can be found in:
+%   
+%   S. Meshgini, A. Aghagolzadeh, H. Seyedarabi, "Face recognition using 
+%   Gabor-based direct linear discriminant analysis and support vector machine," 
+%   Computers & Electrical Engineering, vol. 39, no. 3, pp. 727-745, 2013.
+% 
+% 
+% 
+%   (C)	Saeed Meshgini, Ph.D.
+%       University of Tabriz
+
+
+
+% checking the correct use of input arguments:
+
+if (~exist('options','var'))
+   options.KernelType = 'linear';
+end
+
+% checking the same dimensionality of input vectors:
+
+p = length(u);
+q = length(v);
+if p ~= q
+    error('dimension of two vectors must be the same.')
+end
+
+% kernels:
+
+if ~isfield(options,'KernelType')
+    options.KernelType = 'linear';          % default kernel function is linear
+end
+
+switch lower(options.KernelType)
+    case {'linear'}                 
+%         k = u.'*v;                          % u'*v
+        k = (u')*v;
+    case {'poly'}            
+        if ~isfield(options,'KernelPars')
+            options.KernelPars = 2;         % default value for degree of polynomial is 2
+        end
+        k = ((u')*v).^options.KernelPars;
+%         k = (u.'*v)^options.KernelPars;     % (u'*v)^n
+    case {'polyplus'}                
+        if ~isfield(options,'KernelPars')
+            options.KernelPars = 2;         % default value for degree of polynomial is 2
+        end
+        k = ((u')*v+1).^options.KernelPars;
+%         k = (u.'*v+1)^options.KernelPars;   % (u'*v+1)^n
+    case {'sphnorm_poly'}      
+        if ~isfield(options,'KernelPars')
+            options.KernelPars = [2 1];     % default value for degree of polynomial is 2 and default parameter for spherical normalization is 1
+        end
+        
+        k = (1/2^options.KernelPars(1))*...
+             ((           ((u')*v + options.KernelPars(2)^2)./...
+                 sqrt( (sum(u.^2,1)' + options.KernelPars(2)^2) *...
+                       (sum(v.^2,1) + options.KernelPars(2)^2)   ) )+1).^options.KernelPars(1);
+       
+%         k = (1/2^options.KernelPars(1))*...
+%              ((        (u.'*v+options.KernelPars(2)^2)/...
+%                  sqrt( (u.'*u+options.KernelPars(2)^2)*...
+%                        (v.'*v+options.KernelPars(2)^2) )   )+1)^options.KernelPars(1);
+                                            % ((u'*v+d^2)/sqrt((u'*u+d^2)(v'*v+d^2))+1)^n/2^n
+    case {'rbf'}        
+        if ~isfield(options,'KernelPars')   % default value for sigma is 1
+            options.KernelPars = 1; 
+        end
+        k = zeros(size(u,2),size(u,2));
+        for i=1:size(u,2)
+            for j=1:size(v,2)
+                dTmp = u(:,i)-v(:,j);
+                k(i,j) = exp((-dTmp'*dTmp)/(2*options.KernelPars^2));
+            end
+        end
+%         k = exp((-(u-v).'*(u-v))/(2*options.KernelPars^2));
+                                            % e^{-(|u-v|^2)/2(sigma)^2}
+    case {'wave'}        
+        if ~isfield(options,'KernelPars')   % default value for sigma is 1
+            options.KernelPars = 2 ;        % default value for dilation factor is 2 and default mother wavelet is morlet function
+        end 
+%         if strcmp(options.KernelPars(2),'morl')
+            k = zeros(size(u,2),size(u,2));
+            for i=1:size(u,2)
+                for j=1:size(v,2)
+                    dTmp = u(:,i)-v(:,j);
+                    k(i,j) = prod(cos(1.75*dTmp/options.KernelPars).*exp(-(dTmp.^2)/(2*options.KernelPars^2)));
+                end
+            end
+%             pro = 1;
+%             for i = 1:p
+%                 pro = pro*(cos(1.75*((u(i)-v(i))/options.KernelPars))*exp(-((u(i)-v(i))^2)/(2*options.KernelPars^2)));  
+%             end
+%             k = pro;                        % Pro(psi(ui-vi)/a), psi(x) = cos(1.75x)exp(-x^2/2)
+%         elseif strcmp(options.KernelPars(2),'mexh')
+%             pro = 1;
+%             for i = 1:p
+%                 pro = pro*(((2/sqrt(3))*pi^(-0.25))*(1-((u(i)-v(i))/options.KernelPars(1))^2)*exp((-((u(i)-v(i))/options.KernelPars(1))^2)/2));
+%             end
+%             k = pro;
+%         elseif strcmp(options.KernelPars(2),'haar')
+%             pro = 1;
+%             for i = 1:p
+%                 if (((u(i)-v(i))/options.KernelPars(1))> = 0)&&(((u(i)-v(i))/options.KernelPars(1))<0.5)
+%                     pro = pro*1;
+%                 elseif (((u(i)-v(i))/options.KernelPars(1))> = 0.5)&&(((u(i)-v(i))/options.KernelPars(1))<1)
+%                     pro = pro*(-1);
+%                 else
+%                     pro = pro*0;
+%                 end
+%             end
+%             k = pro;
+%         else
+%             error('use one of these wavelets ("morl" or "mexh" or "haar") as valid wavelet types')
+%         end
+    otherwise
+        error('KernelType does not exist!');
+end
+end
